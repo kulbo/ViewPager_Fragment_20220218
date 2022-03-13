dba2c3827f4c6b8415f42f408b932d811fa24560
이 성 노 <kulbo0926@gmail.com>
2022-03-14 07:23:18 +0900

    생성자를 수정 > FragmentManager 반아서 부모 생성자에 전달

--- app/src/main/java/kr/co/smartsoft/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/kr/co/smartsoft/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
@@ -1,6 +1,8 @@
 package kr.co.smartsoft.viewpager_fragment_20220218.adapters
 
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
-class MainViewPagerAdapter : FragmentPagerAdapter() {
+class MainViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
+
 }
