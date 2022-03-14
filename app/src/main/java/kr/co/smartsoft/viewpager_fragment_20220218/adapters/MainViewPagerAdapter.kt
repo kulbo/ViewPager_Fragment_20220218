3cecf107e6a3b1a5378cba50f97d03711f13d3dd
이 성 노 <kulbo0926@gmail.com>
2022-03-14 09:21:58 +0900

    함수의 결과를 바로 지정

--- app/src/main/java/kr/co/smartsoft/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/kr/co/smartsoft/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
@@ -3,26 +3,19 @@ package kr.co.smartsoft.viewpager_fragment_20220218.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import kr.co.smartsoft.viewpager_fragment_20220218.fragments.BirthYearFragment
-import kr.co.smartsoft.viewpager_fragment_20220218.fragments.HelloFragment
-import kr.co.smartsoft.viewpager_fragment_20220218.fragments.NameFragment
+import kr.co.smartsoft.viewpager_fragment_20220218.fragments.*
 
 class MainViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
-    override fun getCount(): Int {
+    override fun getCount() = 3
 //        총 몇 페이지의 뷰페이저인지 숫자를 알려달라.
-        return 3
-    }
 
     override fun getItem(position: Int): Fragment {
 //        각각의 position에 맞는 프래그먼트 객체가 결과로 나가도록 리턴.
-        if (position == 0) {
-            return NameFragment()
-        }
-        else if (position == 1) {
-            return BirthYearFragment()
-        }
-        else {
-            return HelloFragment()
+
+        return when (position) {
+            0 -> NameFragment()
+            1 -> BirthYearFragment()
+            else -> HelloFragment()
         }
     }
 }
